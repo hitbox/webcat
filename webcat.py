8d39dc8488fdf9783e35f1191f0c6ba919fc1c5d
Carl Harris <hitbox@ryzen.com>
2024-03-07 02:12:36 -0500

    added prefix middleware

--- webcat.py
+++ webcat.py
@@ -6,6 +6,32 @@ from flask import send_file
 
 catbp = Blueprint('cat', __name__)
 
+class PrefixMiddleware:
+    """
+    Require a prefix for all url paths.
+    """
+
+    def __init__(self, app, prefix):
+        self.app = app
+        self.prefix = prefix
+
+    def __call__(self, environ, start_response):
+        """
+        Raise error if url is not properly prefixed. Otherwise, strip prefix
+        and ship off to application's path.
+        """
+        path_info = environ.get('PATH_INFO', '')
+        if path_info.lower().startswith(self.prefix.lower()):
+            # strip the prefix off the url path
+            environ['PATH_INFO'] = path_info[len(self.prefix):]
+            environ['SCRIPT_NAME'] = self.prefix
+            return self.app(environ, start_response)
+        else:
+            start_response('404', [('Content-Type', 'text/plain')])
+            return [ "This url does not belong to the app."
+                    f" Expected prefix {self.prefix!r}".encode()]
+
+
 @catbp.route('/favicon')
 def favicon():
     """
@@ -29,6 +55,12 @@ def create_app():
     Application factory.
     """
     app = Flask(__name__, instance_relative_config=True)
+
     app.config.from_envvar('WEBCAT_INSTANCE_RELATIVE_CONFIG')
+
+    prefix = app.config.get('PREFIX')
+    if prefix:
+        app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix)
+
     app.register_blueprint(catbp)
     return app
