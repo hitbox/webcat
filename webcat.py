9a3c5afb4d59a70d9f2b29a39303b42a2b955479
Carl Harris <hitbox@ryzen.com>
2024-03-13 04:54:23 -0400

    better streaming
    - read metadata from config to generate links to database results
    - fake data generators for development

--- webcat.py
+++ webcat.py
@@ -1,8 +1,12 @@
+import random
+import time
+
 import sqlalchemy as sa
 
 from flask import Blueprint
 from flask import Flask
 from flask import current_app
+from flask import request
 from flask import send_file
 from flask import stream_template
 
@@ -62,12 +66,19 @@ def database_data_from_config():
     servers = current_app.config['WEBCAT_SERVERS']
     queries = current_app.config['WEBCAT_SHOW_QUERIES']
 
-    results = []
-    for querydata in queries:
-        url = servers[querydata['server']]
-        engine = sa.create_engine(url)
-        results.append(database_result(engine, querydata))
-    return results
+    if servers is None or queries is None:
+        # slow generator for development
+        return slow_database_results()
+    else:
+        # TODO
+        # - scoped session for app context
+        # - streamable instead of `all`
+        results = []
+        for querydata in queries:
+            url = servers[querydata['server']]
+            engine = sa.create_engine(url)
+            results.append(database_result(engine, querydata))
+        return results
 
 @catbp.route('/favicon')
 def favicon():
@@ -78,6 +89,44 @@ def favicon():
     # - trying to keep anything instance specific out of repo
     return send_file(current_app.config['WEBCAT_FAVICON'])
 
+def slow_rows(delayfunc, nrows, nfields, prefix=''):
+    """
+    Generate fake rows with a delay.
+    """
+    for row_num in range(nrows):
+        yield tuple(prefix + f'value-{valn}-{row_num}' for valn in range(nfields))
+        time.sleep(delayfunc())
+
+def slow_database_results():
+    """
+    Fake slow database connection.
+    """
+    for querydata in current_app.config['WEBCAT_SHOW_QUERIES']:
+        id = querydata['id']
+        nfields = querydata['_nfields']
+        nrows = querydata['_nrows']
+
+        if '_delay' in querydata and '_random_delay' in querydata:
+            raise ValueError('incompatible keys')
+        elif '_delay' in querydata:
+            delay = querydata['_delay']
+            delayfunc = lambda: delay
+        elif '_random_delay' in querydata:
+            a, b = querydata['_random_delay']
+            def delayfunc():
+                return random.uniform(a, b)
+        else:
+            delayfunc = lambda: 0
+
+        yield dict(
+            title = querydata['title'],
+            id = id,
+            result = dict(
+                fieldnames = [f'{id}-field-{fieldn}' for fieldn in range(nfields)],
+                rows = slow_rows(delayfunc, nrows, nfields, prefix=f'{id}-'),
+            ),
+        )
+
 @catbp.route('/')
 def output():
     """
@@ -85,6 +134,7 @@ def output():
     """
     with open(current_app.config['WEBCAT_FILE']) as webcat_file:
         file_content = webcat_file.read()
+
     database_results = database_data_from_config()
     context = dict(
         file_content = file_content,
